ee8b18066aa772e8c73cc6e02924fdc6a2678692
Tim Hughes <tzhughes@gmail.com>
2014-05-07 11:40:22 +0200

    Clarifying running of tests

--- tests/fileLevelTests/test_famFile/runTests.bash
+++ tests/fileLevelTests/test_famFile/runTests.bash
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+
 # Testing whether non-standard FAM files are handled appropriately
 
 # Call with FAM file
 function run(){
 echo "######################## ${1}"
 cat ${1}
-echo -e "\n\n"
+echo -e "\n"
 ../../../scripts/run_genmod.py -phased ${1} oneARvariant.vcf
-echo -e "\n\n\n\n"
+echo -e "\n\n\n"
 }
 
 
-echo "SHOULD RUN THROUGH"
+echo "FOLLOWING SHOULD RUN"
 run standardTrio.fam
 
-
-echo "SHOULD THROW EXCEPTION"
+echo "FOLLOWING SHOULD THROW EXCEPTION"
 run standardTrio_extraHealthySister.fam
+
+echo "FOLLOWING SHOULD THROW EXCEPTION"
 run standardTrio_missingFather.fam
+
+echo "FOLLOWING SHOULD THROW EXCEPTION"
 run standardTrio_noRelationships.fam
+
+echo "FOLLOWING SHOULD THROW EXCEPTION"
 run standardTrio_missingColForSon.fam
+
+echo "FOLLOWING SHOULD THROW EXCEPTION"
 run standardTrio_extraColForSon.fam
+
+echo "FOLLOWING SHOULD THROW EXCEPTION"
 run standardTrio_sexInversionOnParents.fam
 
 
