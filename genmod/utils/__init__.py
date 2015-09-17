b1707c7c2636c474b81088793326175d8d18bbf7
moonso <monsunas@gmail.com>
2015-09-17 14:30:57 +0200

    Solved merge conflicts in utils/__init__

--- genmod/utils/__init__.py
+++ genmod/utils/__init__.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-from __future__ import print_function
-from genmod.utils.load_annotation import load_annotations
-from genmod.utils.is_number import is_number
-from genmod.utils.plugin_reader import collectKeys
-from genmod.utils.sort_variants import sort_variants
-from genmod.utils.print_variants import print_variants, print_variant_for_sorting
-from genmod.utils.print_headers import print_headers
-from genmod.utils.add_metadata import add_metadata
-from genmod.utils.pair_generator import PairGenerator
-=======
 from __future__ import (print_function, absolute_import)
 
 INTERESTING_SO_TERMS = set(
@@ -71,4 +60,3 @@ from .get_features import (get_annotation, check_vep_annotation, check_exonic)
 from .get_batches import get_batches
 from .get_priority import (get_chromosome_priority, get_rank_score)
 from .variant_printer import VariantPrinter
->>>>>>> feature/fix_compounds_single
