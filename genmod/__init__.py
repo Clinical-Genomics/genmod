c62ef1707faaf485af01dfda9541c81c1b9beaff
moonso <monsunas@gmail.com>
2015-09-17 14:08:27 +0200

    Removed files and updated log

--- genmod/__init__.py
+++ genmod/__init__.py
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-from __future__ import print_function
-
-import logging
-logger = logging.getLogger(__name__)
-
-import pkg_resources
-__version__ = pkg_resources.require("genmod")[0].version
-
-from genmod.annotation_parser import AnnotationParser
-from genmod.genetic_models import check_genetic_models
-from genmod.variant_consumer import VariantConsumer
-from genmod.variant_scorer import VariantScorer
-from genmod.variant_printer import VariantPrinter
-from genmod.get_batches import get_batches
-from genmod.utils import (sort_variants, load_annotations, is_number,
-    collectKeys, print_variants, print_headers, add_metadata, PairGenerator)
-from genmod.errors import warning
-=======
 from __future__ import (print_function, unicode_literals, absolute_import)
 from logging import getLogger
 from pkg_resources import require
->>>>>>> feature/fix_compounds_single
 
 logger = getLogger(__name__)
 
--- genmod/log.py
+++ genmod/log.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import os
->>>>>>> feature/fix_compounds_single
 import sys
 import logging
 
@@ -11,10 +8,6 @@ LEVELS = {
     2 : 'DEBUG',
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> feature/fix_compounds_single
 def init_log(logger, filename=None, loglevel=None):
     """
     Initializes the log file in the proper format.
@@ -50,27 +43,15 @@ def init_log(logger, filename=None, loglevel=None):
 
     logger.addHandler(console)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> feature/fix_compounds_single
 def get_log_stream(logger):
     """
     Returns a stream to the root log file.
     If there is no logfile return the stderr log stream
-<<<<<<< HEAD
-    
-    Returns:
-        A stream to the root log file or stderr stream.
-    """
-    
-=======
 
     Returns:
         A stream to the root log file or stderr stream.
     """
 
->>>>>>> feature/fix_compounds_single
     file_stream = None
     log_stream = None
     for handler in logger.handlers:
@@ -78,15 +59,8 @@ def get_log_stream(logger):
             file_stream = handler.stream
         else:
             log_stream = handler.stream
-<<<<<<< HEAD
-    
-    if file_stream:
-        return file_stream
-    
-=======
 
     if file_stream:
         return file_stream
 
->>>>>>> feature/fix_compounds_single
     return log_stream
