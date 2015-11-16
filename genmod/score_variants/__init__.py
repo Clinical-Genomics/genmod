9dcbe0fbd92784faea18545f8fbe22df5edac5fc
moonso <monsunas@gmail.com>
2015-11-16 13:58:55 +0100

    Added test if all plugins are defined in vcf file

--- genmod/score_variants/__init__.py
+++ genmod/score_variants/__init__.py
@@ -4,3 +4,4 @@ from .score_function import ScoreFunction
 from .config_parser import ConfigParser
 from .score_variant import score_variant, get_category_score
 from .compound_scorer import CompoundScorer
+from .check_plugins import check_plugins
