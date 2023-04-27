07357512df790353c72c17dddfbdc11750599464
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-04-27 15:59:10 +0200

    Genmod __version__: Allow testing of module without pkg install
    When running pytest, initialisation of the genmod module fails
    due to it not being installed as a proper package in container.
    
    Add an environment variable check that avoids checking
    package installed genmod version, by supplying a
    dummy one for docker testing purposes.
    
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- genmod/__init__.py
+++ genmod/__init__.py
@@ -1,7 +1,14 @@
 from __future__ import (print_function, absolute_import)
 from logging import getLogger
-from pkg_resources import get_distribution
+from pkg_resources import get_distribution, DistributionNotFound
+from os import environ
 
 logger = getLogger(__name__)
 
-__version__ = get_distribution("genmod").version
+try:
+    __version__ = get_distribution("genmod").version
+except DistributionNotFound as error:
+    # Genmod is not installed (as the case for docker test suite)
+    if 'GENMOD_DOCKER_TEST' not in environ.keys():
+        raise error
+    __version__ = '-1.-1.-1'
