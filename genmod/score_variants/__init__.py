e8808360b0a199dc5c68f93c5b861ca65c2c28c3
Tor Björgen <tor.bjorgen@scilifelab.se>
2023-05-02 10:09:04 +0200

    Remove score_variant() method in favor of score()
    There are multiple implementations of the scoring interface,
    i.e. wrapper methods that use the get_category_score().
    
    In order to reduce ambiguity, remove the score_variant()
    method (that currently has no callee) in favor of
    the score() method in commands/score_variants.py.
    
    Signed-off-by: Tor Björgen <tor.bjorgen@scilifelab.se>

--- genmod/score_variants/__init__.py
+++ genmod/score_variants/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from .score_function import ScoreFunction
 from .config_parser import ConfigParser
-from .score_variant import score_variant, get_category_score
+from .score_variant import get_category_score
 from .compound_scorer import CompoundScorer
 from .check_plugins import check_plugins
