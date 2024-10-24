3db7172e7f9d8e563331436760f554c14590fd88
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-10-24 13:27:56 +0200

    spelling

--- genmod/annotations/__init__.py
+++ genmod/annotations/__init__.py
@@ -3,5 +3,5 @@ from importlib_resources import files
 ensembl_file_37 = 'annotations/ensembl_genes_37.txt.gz'
 ensembl_file_38 = 'annotations/ensembl_genes_38.txt.gz'
 
-ensembl_path_37 = files('genmod').join_path(ensembl_file_37)
-ensembl_path_38 = files('genmod').join_path(ensembl_file_38)
+ensembl_path_37 = files('genmod').joinpath(ensembl_file_37)
+ensembl_path_38 = files('genmod').joinpath(ensembl_file_38)
