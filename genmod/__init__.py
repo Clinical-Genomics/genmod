0f87729baf5dae221441e457e6c856ca6276b60e
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-10-24 13:31:05 +0200

    restore logger

--- genmod/__init__.py
+++ genmod/__init__.py
@@ -1,6 +1,10 @@
+from logging import getLogger
+
 try:
     from importlib.metadata import version
 except ImportError:  # Backport support for importlib metadata on Python 3.7
     from importlib_metadata import version
+
+logger = getLogger(__name__)
 
 __version__ = version("genmod")
