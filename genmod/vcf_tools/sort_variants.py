5a7b4770fa1cca2ac1cde086700748cd86bb9b2e
fellen31 <felixlenner@gmail.com>
2025-04-14 08:50:11 +0200

    Fix stable sort in bioconda

--- genmod/vcf_tools/sort_variants.py
+++ genmod/vcf_tools/sort_variants.py
@@ -39,7 +39,7 @@ def sort_variants(infile, mode="chromosome"):
     if mode == "chromosome":
         command.append("-k1,1V")  # Version sorting to deal with e.g. Un_* contigs
         command.append("-k3,3n")  # Sorting positions numerically
-        command.append("--stable")
+        command.append("-s")
 
     elif mode == "rank":
         command.append("-rn")
