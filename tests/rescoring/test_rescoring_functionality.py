49d8a435232faf2df6c0f6458df61b7bf13633ce
Tor Björgen <tor.bjorgen@scilifelab.se>
2025-06-26 13:32:45 +0200

    fixup! Add test for rescoring with additional suffix

--- tests/rescoring/test_rescoring_functionality.py
+++ tests/rescoring/test_rescoring_functionality.py
@@ -1,6 +1,8 @@
 from click.testing import CliRunner
+from tempfile import NamedTemporaryFile
+
 from genmod import logger
-from genmod.commands import score_command
+from genmod.commands import score_command, score_compounds_command
 from genmod.log import init_log
 
 SCORED_VCF = "tests/fixtures/test_vcf_annotated_scored.vcf"
@@ -18,15 +20,26 @@ def test_rescore_with_annotation_suffix():
         - All previously existing GENMOD annotations are present and unaltered
         - New scoring annotations are added that's based on an additional scoring config
     """
-    runner = CliRunner()
-    #r = runner.invoke(score_command, ['--help'])
-    #print(r.output)
-    #assert False
 
+    # Variant Scoring
+    runner = CliRunner()
     result = runner.invoke(score_command,
                            [SCORED_VCF,
                             "-c", SCORE_CONFIG,
                             "--skip_is_previously_scored_check"])
-
-    print(result.output)
     assert result.exit_code == 0
+    temporary_file = NamedTemporaryFile()
+    with open(temporary_file.name, "w") as file:
+        file.write(result.stdout_bytes.decode("utf-8"))  # Save processed VCF to file
+
+    # Compound Scoring
+    runner = CliRunner()
+    # WHEN computing compound score
+    result = runner.invoke(
+        score_compounds_command,
+        [temporary_file.name],
+    )
+    assert result.exit_code == 0
+    temporary_file = NamedTemporaryFile()
+    with open(temporary_file.name, "w") as file:
+        file.write(result.stdout_bytes.decode("utf-8"))  # Save processed VCF to file
